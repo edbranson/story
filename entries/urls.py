bf2d27e2c90779cfff1873ae02838952397b4e94
Ed Branson <ed@bransons.com>
2022-05-31 12:45:41 -0600

    20220531b

--- entries/urls.py
+++ entries/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name ='home'),
     path('entry/create/<str:med>/', views.EntryCreate.as_view(), name='entry-create'),
-    path('entrymedia/create/', views.EntryMediaCreate.as_view(), name='entrymedia-create'),
+    # path('entrymedia/create/', views.EntryMediaCreate.as_view(), name='entrymedia-create'),
     path('entry/list/<str:col>/', views.EntryList.as_view(), name="entry-list"),
     path('entry/edit/<str:pk>/', views.EntryEdit.as_view(), name="entry-edit"),
     path('entry/delete/<str:pk>/', views.EntryDelete.as_view(), name="entry-delete"),
